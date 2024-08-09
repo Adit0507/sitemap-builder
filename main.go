d36cefa1d91b29a50258f2272340bd1025be8206
Adit0507 <adi05072002@gmail.com>
2024-08-09 16:20:19 +0530

    filtering & refactoring

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"sitemap/link"
@@ -13,12 +14,18 @@ func main() {
 	urlFlag := flag.String("url", "https://aditya-portfolio-ten.vercel.app/", "the url I want to build a sitemap for")
 	flag.Parse()
 
-	resp, err := http.Get(*urlFlag)
+	fmt.Println(*urlFlag)
+	pages := get(*urlFlag)
+	for _, page := range pages {
+		fmt.Println(page)
+	}
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
 	if err != nil {
 		panic(err)
 	}
-
-	defer resp.Body.Close()
 
 	reqUrl := resp.Request.URL
 	baseUrl := &url.URL{
@@ -26,20 +33,36 @@ func main() {
 		Host:   reqUrl.Host,
 	}
 	base := baseUrl.String()
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+}
 
-	links, _ := link.Parse(resp.Body)
-	var hrefs []string
+func hrefs(body io.Reader, base string) []string {
+	links, _ := link.Parse(body)
+	var ret []string
 	for _, l := range links {
 		switch {
 		case strings.HasPrefix(l.Href, "/"):
-			hrefs = append(hrefs, base + l.Href)
+			ret = append(ret, base+l.Href)
 		case strings.HasPrefix(l.Href, "http"):
-			hrefs = append(hrefs, l.Href)	
+			ret = append(ret, l.Href)
 		}
 	}
 
-	for _, href := range hrefs{
-		fmt.Println(href)
-	}
+	return ret
+}
 
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, link := range links{
+		if keepFn(link){
+			ret = append(ret, link)
+		}
+	}
+	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
+	}
 }
