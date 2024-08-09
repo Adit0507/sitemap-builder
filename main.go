f88106cf2d51558f03c0f393071f4cda7a55d620
Adit0507 <adi05072002@gmail.com>
2024-08-09 14:16:57 +0530

    parsing and building links

--- main.go
+++ main.go
@@ -3,22 +3,43 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
+	"net/url"
+	"sitemap/link"
+	"strings"
 )
 
 func main() {
-	urlFlag:= flag.String("url", "https://letterboxd.com/", "the url I want to build a sitemap for")
+	urlFlag := flag.String("url", "https://aditya-portfolio-ten.vercel.app/", "the url I want to build a sitemap for")
 	flag.Parse()
 
-	fmt.Println(*urlFlag)
 	resp, err := http.Get(*urlFlag)
-	if err!= nil{
+	if err != nil {
 		panic(err)
 	}
 
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+	links, _ := link.Parse(resp.Body)
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base + l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)	
+		}
+	}
+
+	for _, href := range hrefs{
+		fmt.Println(href)
+	}
 
 }
