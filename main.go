dca18ca635faa603387db94b6f93adf5517da8d5
Adit0507 <adi05072002@gmail.com>
2024-08-10 11:30:26 +0530

    implementing bfs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	// "encoding/base32"
 	"flag"
 	"fmt"
 	"io"
@@ -11,22 +12,51 @@ import (
 )
 
 func main() {
-	urlFlag := flag.String("url", "https://aditya-portfolio-ten.vercel.app/", "the url I want to build a sitemap for")
+	urlFlag := flag.String("url", "https://letterboxd.com", "the url I want to build a sitemap for")
+	maxDepth := flag.Int("depth", 10, "the maximum no. of links deep to traverse")
 	flag.Parse()
 
-	fmt.Println(*urlFlag)
-	pages := get(*urlFlag)
+	pages := bfs(*urlFlag, *maxDepth)
 	for _, page := range pages {
 		fmt.Println(page)
 	}
 }
 
+func bfs(urlStr string, maxDepth int) []string {
+	seen := make(map[string]struct{})
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		urlStr: struct{}{},
+	}
+	for i := 0; i <= maxDepth; i++ {
+		q, nq = nq, make(map[string]struct{})
+		if len(q) == 0 {
+			break
+		}
+		for url, _ := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = struct{}{}
+			for _, link := range get(url) {
+				nq[link] = struct{}{}
+			}
+		}
+	}
+	ret := make([]string, 0, len(seen))
+	for url, _ := range seen {
+		ret = append(ret, url)
+	}
+	return ret
+}
+
 func get(urlStr string) []string {
 	resp, err := http.Get(urlStr)
 	if err != nil {
-		panic(err)
+		return []string{}
 	}
 
+	defer resp.Body.Close()
 	reqUrl := resp.Request.URL
 	baseUrl := &url.URL{
 		Scheme: reqUrl.Scheme,
@@ -53,8 +83,8 @@ func hrefs(body io.Reader, base string) []string {
 
 func filter(links []string, keepFn func(string) bool) []string {
 	var ret []string
-	for _, link := range links{
-		if keepFn(link){
+	for _, link := range links {
+		if keepFn(link) {
 			ret = append(ret, link)
 		}
 	}
